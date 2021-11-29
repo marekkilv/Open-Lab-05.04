7d6b43bd35ee69a31a7b1a201ee1ede7ab5c830e
Marekmp@outlook.sk <Marekmp@outlook.sk>
2021-11-29 20:12:30 +0100

    mam

--- Open-Lab-05.04/StringTools.cs
+++ Open-Lab-05.04/StringTools.cs
@@ -6,7 +6,16 @@ namespace Open_Lab_05._04
     {
         public string RemoveSpecialCharacters(string original)
         {
-            throw new NotImplementedException();
+           Regex rgx = new Regex("[^A-Za-z0-9 -]");
+            string new_string = string.Empty;
+            for (int i = 0; i <= original.Length-1; i++)
+            {
+                if (!rgx.IsMatch(original[i].ToString()))
+                {
+                    new_string += original[i];
+                }
+            }
+            return new_string;
         }
     }
 }
